1864f7b52e36544a0873cd48e8edcf38b23919d6
OGHyperion <astilekuriakose97@gmail.com>
2020-06-07 11:47:30 +0530

    curtana: import some Blobs from MIUI 11.0.8.0 dump import: qti_whitelist.xml import: privapp-permissions-qti.xml import: telephony_product_privapp-permissions-qti.xml

--- device.mk
+++ device.mk
@@ -19,6 +19,18 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+# QTI
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-qti.xml \
+
+# Low power Whitelist
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml \
+
+# QCOM
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/telephony_product_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/telephony_product_privapp-permissions-qti.xml \
+
 # Soong
 PRODUCT_USES_QCOM_HARDWARE := true
 PRODUCT_BOARD_PLATFORM := sm6250
